7523c3c1756228dabe0f879c6980ed91a4eac298
B7b5 <urbanspecific@gmail.com>
2025-02-22 10:58:44 -0500

    Update Navbar and SubNav components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,33 +4,26 @@ import SubNav from './SubNav';
 import navItems from '../nav-data';
 
 function Navbar() {
-  const [openSubNav, setOpenSubNav] = useState(null);
   const [hoveredNavItem, setHoveredNavItem] = useState(null);
 
   return (
     <MainNav>
       <NavContent>
-        <div style={{ position: 'fixed', left: '40px' }}> 
+        <div style={{ position: 'fixed', left: '40px' }}>
           <Logo href="/">Gitan</Logo>
         </div>
         <NavList>
           {navItems.map((item) => (
             <NavItem
               key={item.text}
-              onMouseEnter={() => {
-                setOpenSubNav(item.text); // Update openSubNav on mouse enter
-                setHoveredNavItem(item.text);
-              }}
-              onMouseLeave={() => {
-                setOpenSubNav(null); // Reset openSubNav on mouse leave
-                setHoveredNavItem(null);
-              }}
+              onMouseEnter={() => setHoveredNavItem(item.text)}
+              onMouseLeave={() => setHoveredNavItem(null)}
             >
-              <NavLink href={item.href} $isHovered={hoveredNavItem && hoveredNavItem!== item.text}>
+              <NavLink href={item.href} $isHovered={hoveredNavItem && hoveredNavItem !== item.text}>
                 {item.text}
               </NavLink>
-              {/* Conditionally render SubNav based on openSubNav state */}
-              {item.subnav && openSubNav === item.text && (
+              {/* Always render SubNav if item has subnav data */}
+              {item.subnav && (
                 <SubNav $isopen={true} items={item.subnav} />
               )}
             </NavItem>
--- src/components/SubNav.jsx
+++ src/components/SubNav.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SubNavContainer = styled.div`
-  position: absolute;
-  top: 100%;
-  left: 0;
-  background-color: pink;
-  padding: 10px;
-  display: ${props => props.$isopen? 'block': 'none'};
-  z-index: 101;
-`;
-
 const SubNavList = styled.ul`
   list-style: none;
   margin: 0;
@@ -26,17 +16,15 @@ const SubNavLink = styled.a`
   color: #333;
 `;
 
-function SubNav({ $isopen, items }) {
+function SubNav({ items }) {
   return (
-    <SubNavContainer $isopen={$isopen}>
-      <SubNavList>
-        {items.map((item, index) => (
-          <SubNavItem key={index}>
-            <SubNavLink href={item.href}>{item.text}</SubNavLink>
-          </SubNavItem>
-        ))}
-      </SubNavList>
-    </SubNavContainer>
+    <SubNavList className="subnav">
+      {items.map((item, index) => (
+        <SubNavItem key={index}>
+          <SubNavLink href={item.href}>{item.text}</SubNavLink>
+        </SubNavItem>
+      ))}
+    </SubNavList>
   );
 }
 
