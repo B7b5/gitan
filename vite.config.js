e2c499b1579260dfd03ae334c7e81458841754d2
B7b5 <urbanspecific@gmail.com>
2025-02-25 09:25:37 -0500

    Subnav fix to dist

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/main.jsx',
+        styles: 'src/index.css' // Force Vite to include it
+      }
+    }
+  },
+  css: {
+    postcss: {
+      plugins: []
+    }
+  }
+});
